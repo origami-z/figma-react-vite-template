a05e1a8335460cc813f490dfa1b5aa7126a95e45
origami-z <davidcui2@me.com>
2023-01-28 23:38:54 +0000

    Bump build tool dependencies

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,5 @@
 import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import react from "@vitejs/plugin-react-swc";
 import { viteSingleFile } from "vite-plugin-singlefile";
 
 // https://vitejs.dev/config/
@@ -11,13 +11,6 @@ export default defineConfig({
     assetsInlineLimit: 100000000,
     chunkSizeWarningLimit: 100000000,
     cssCodeSplit: false,
-    brotliSize: false,
     outDir: "../dist",
-    rollupOptions: {
-      inlineDynamicImports: true,
-      output: {
-        manualChunks: () => "everything.js",
-      },
-    },
   },
 });
