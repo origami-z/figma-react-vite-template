e6b982b4ff6e6073e8dbd883f65fb32c9ebf7c77
origami-z <5257855+origami-z@users.noreply.github.com>
2026-03-22 22:58:16 +0000

    chore: migreate to use viteplus

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,19 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react-swc";
+import { defineConfig } from "vite-plus";
+import react from "@vitejs/plugin-react";
 import { viteSingleFile } from "vite-plugin-singlefile";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  fmt: {
+    printWidth: 80,
+    tabWidth: 2,
+    sortPackageJson: false,
+    ignorePatterns: [],
+  },
+  staged: {
+    "*": "vp check --fix",
+  },
+  lint: { options: { typeAware: true, typeCheck: true } },
   root: "./ui-src",
   plugins: [react(), viteSingleFile()],
   build: {
